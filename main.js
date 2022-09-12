3425ef969b93bda7d857c9a70e736538757ceaeb
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-09-12 18:37:00 +0200

    finished email error system and add icon in input

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let errorAlert = document.querySelector('.false-email');
-errorAlert.style.display = 'none'
 
 function validEmail() {
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     let emailInput = document.querySelector('.form');
+    let iconError = document.querySelector('.icon-error');
     
     emailInput.addEventListener('change', () => {
 
         if (!emailInput.value.match(emailPattern)) {
-            emailInput.classList.add('valid');
+            emailInput.classList.add('invalid');
+            errorAlert.style.display = 'inherit'
+            iconError.style.display = 'inherit'
 
             if (emailInput.value == '') {
-                emailInput.classList.remove('valid');
+                emailInput.classList.remove('invalid');
+                errorAlert.style.display = 'none'
+                iconError.style.display = 'none'
+
             } else return;
         }   
     })
